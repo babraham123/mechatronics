7a2cb78de8ce1202fad08003a29a3b08a34270a5
Bereket Abraham <babraham42@gmail.com>
2016-03-03 22:31:51 -0500

    vacuum testing

---
+++ arduino/vacuumSensor/vacuumSensor.ino
@@ -0,0 +1,14 @@
+// test the vacuum sensor
+// Bereket Abraham
+
+const int vacuumPin = A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(vacuumPin);
+  Serial.println(sensorValue);
+  delay(100);
+}
