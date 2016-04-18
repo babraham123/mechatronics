04f3cf84a3f18526b5791cf6434a6699c738cd9f
Bereket Abraham <babraham42@gmail.com>
2016-04-18 00:47:30 -0400

    robot control v4

--- arduino/RobotControlV4/RobotControlV4.ino
+++ arduino/RobotControlV4/RobotControlV4.ino
@@ -1,14 +1,20 @@
-/*
+/* Icarus Window Washer, V4.0
+ * Mechatronic Design
+ * Roy Shin, Bereket Abraham
+ */
+
+/* Manual Control Scheme
+
    Movement: WASD, x to center Y
    X legs: i to raise, k to lower
    Y legs: o to raise, l to lower
    Pressure: p to display pressure
-
 */
 
 #include <Encoder.h>
 #include <NewPing.h>
 
+/********** Pin Definitions ***********/
 const int pinLimLT = 19;
 const int pinLimRT = 21;
 const int pinLimUP = 20;
@@ -51,43 +57,63 @@ NewPing sonarDN(trigDN, echoDN, 8);
 
 Encoder yEncoder(3, 2);
 
-volatile int prev, curr, next;
-const int S_MOVELT = 0;
-const int S_MOVERT = 1;
-const int S_MOVEUP = 2;
-const int S_MOVEDN = 3;
-const int S_LOWERX = 4;
-const int S_LIFT_X = 5;
-const int S_LOWERY = 6;
-const int S_LIFT_Y = 7;
-const int S_STOPALL = 8;
-const int S_CENTERY = 9;
-const int S_DISPLAY = 10;
-const int S_WAITVACX = 11;
-const int S_WAITVACY = 12;
-
 const long hiThresh = 10;
 const long loThresh = -25;
 const long loTarget = 245;
 const long hiTarget = 265;
 
+/********** States ***********/
+// Performs actions and state transitions. State can override its manager 
+// in case of failure / unexpected conditions. State can transition to a 
+// different manager as well.
+volatile int currState;
+volatile unsigned long stateCounter; // # of cycles since currState started
+const int S_MOVE_LT = 0;
+const int S_MOVE_RT = 1;
+const int S_MOVE_UP = 2;
+const int S_MOVE_DN = 3;
+const int S_LOWER_X = 4;
+const int S_LIFT_X = 5;
+const int S_LOWER_Y = 6;
+const int S_LIFT_Y = 7;
+const int S_STOP_ALL = 8;
+const int S_CENTER_X = 9;
+const int S_CENTER_Y = 10;
+const int S_WAIT_VAC_X = 11;
+const int S_WAIT_VAC_Y = 12;
+
+/********** Managers ***********/
+// A sequence of states that repeats in a loop. Manager can be started in
+// the middle of a sequence
+volatile int *currManager;
+volatile int currIndex;
+int M_TRANSLATE_LT[9] = {S_LOWER_Y, S_WAIT_VAC_Y, S_LIFT_X, S_MOVE_RT, S_LOWER_X, S_WAIT_VAC_X, S_LIFT_Y, S_MOVE_LT, -1};
+int M_TRANSLATE_RT[9] = {S_LOWER_Y, S_WAIT_VAC_Y, S_LIFT_X, S_MOVE_LT, S_LOWER_X, S_WAIT_VAC_X, S_LIFT_Y, S_MOVE_RT, -1};
+int M_TRANSLATE_UP[9] = {S_LOWER_X, S_WAIT_VAC_X, S_LIFT_Y, S_MOVE_DN, S_LOWER_Y, S_WAIT_VAC_Y, S_LIFT_X, S_MOVE_UP, -1};
+int M_TRANSLATE_DN[9] = {S_LOWER_X, S_WAIT_VAC_X, S_LIFT_Y, S_MOVE_UP, S_LOWER_Y, S_WAIT_VAC_Y, S_LIFT_X, S_MOVE_DN, -1};
+// TODO: M_TURN_DN_LT, M_TURN_DN_RT, M_TURN_LT_DN, M_TURN_LT_UP
+// TODO: M_DIVIDER_DN, M_DIVIDER_LT 
+// (robot starts in upper right??)
+int M_STOP[2] = {S_STOP_ALL, -1};
+
+/********** Display Modes ***********/
+// Determines what is outputted to the Serial port by the actions
+volatile int currDisplayMode;
+const int D_NONE = 0;
+const int D_GUI = 1;
+const int D_DEBUG = 2;
+
 /* Global Variables */
 volatile bool limitLT, limitRT, limitUP, limitDN;
-bool printStatus;
-bool movingRight;
-bool stateEnd;
-long initial, current;
-volatile int yExtreme, yPosition;
+volatile int yExtreme, yPosition, xExtreme, xPosition;
 
 void setup() {
   Serial.begin(9600);
-
-  movingRight = true;
-  printStatus = false;
-
-  prev = S_STOPALL;
-  curr = S_STOPALL;
-  next = S_STOPALL;
+  currState = S_STOP_ALL;
+  currDisplayMode = D_GUI;
+  currIndex = 0;
+  stateCounter = 0;
+  currManager = M_STOP;
 
   pinMode(pinLimLT, INPUT);
   pinMode(pinLimRT, INPUT);
@@ -122,264 +148,198 @@ void setup() {
 }
 
 void loop() {
-  int inByte;
+  stateCounter++;
+
+  /********** Manual Control ***********/
   if (Serial.available() > 0) {
-    inByte = Serial.read();
-    switch (inByte) {
-      case 97: // 'a'
-        prev = curr;
-        curr = S_MOVELT;
-        next = S_STOPALL;
+    switch (Serial.read()) {
+      case 'a':
+        currIndex = -1;
+        currManager = M_TRANSLATE_LT;
+        incrementState();
         break;
 
-      case 100: // 'd'
-        prev = curr;
-        curr = S_MOVERT;
-        next = S_STOPALL;
+      case 'd':
+        currIndex = -1;
+        currManager = M_TRANSLATE_RT;
+        incrementState();
         break;
 
-      case 119: // 'w'
-        prev = curr;
-        curr = S_MOVEUP;
-        next = S_STOPALL;
+      case 'w':
+        currIndex = -1;
+        currManager = M_TRANSLATE_UP;
+        incrementState();
         break;
 
-      case 115: // 's'
-        prev = curr;
-        curr = S_MOVEDN;
-        next = S_STOPALL;
+      case 's':
+        currIndex = -1;
+        currManager = M_TRANSLATE_DN;
+        incrementState();
         break;
 
-      case 105: // 'i'
-        prev = curr;
-        curr = S_LIFT_X;
-        next = S_STOPALL;
+      case 'i':
+        currState = S_LIFT_X;
+        currManager = M_STOP;
         break;
 
-      case 107: // 'k'
-        prev = curr;
-        curr = S_LOWERX;
-        next = S_STOPALL;
+      case 'k':
+        currState = S_LOWER_X;
+        currManager = M_STOP;
         break;
 
-      case 111: // 'o'
-        prev = curr;
-        curr = S_LIFT_Y;
-        next = S_STOPALL;
+      case 'o':
+        currState = S_LIFT_Y;
+        currManager = M_STOP;
         break;
 
-      case 108: // 'l'
-        prev = curr;
-        curr = S_LOWERY;
-        next = S_STOPALL;
+      case 'l':
+        currState = S_LOWER_Y;
+        currManager = M_STOP;
         break;
 
-      case 112: // 'p'
+      case 'p':
         displayPressure();
         break;
 
-      case 120: // 'x'
-        prev = curr;
-        curr = S_CENTERY;
-        next = S_STOPALL;
+      case 'x':
+        currState = S_CENTER_X;
+        currManager = M_STOP;
         break;
 
-      case 122: // 'z'
-        prev = curr;
-        curr = S_LOWERX;
-        next = S_WAITVACX;
+      case 'y':
+        currState = S_CENTER_Y;
+        currManager = M_STOP;
         break;
 
-      case 101: // 'e'
-        prev = curr;
-        curr = S_DISPLAY;
-        next = S_STOPALL;
+      case 'e':
+        displayUltrasonic();
         break;
 
-      case 113: // 'q'
-        printStatus = !printStatus;
+      case 'q':
+        currDisplayMode = D_DEBUG;
         break;
 
       default:
-        prev = curr;
-        curr = S_STOPALL;
-        next = S_STOPALL;
+        currState = S_STOP_ALL;
+        currManager = M_STOP;
         break;
     }
   }
 
-  switch (curr) {
-    case S_MOVELT:
-      stateEnd = moveLT();
-      if (stateEnd) {
-        prev = curr;
-        curr = next;
-        switch (next) {
-          case S_STOPALL:
-            next = S_STOPALL;
-            break;
-
-          case S_LOWERX:
-            next = S_WAITVACX;
-            break;
-        }
+  /********** States and Transitions ***********/
+  switch (currState) {
+    case S_MOVE_LT:
+      if (moveLT()) {
+        incrementState();
       }
       break;
 
-    case S_MOVERT:
-      stateEnd = moveRT();
-      if (stateEnd) {
-        prev = curr;
-        curr = next;
-        switch (next) {
-          case S_STOPALL:
-            next = S_STOPALL;
-            break;
-
-          case S_LOWERY:
-            next = S_WAITVACY;
-            break;
-        }
+    case S_MOVE_RT:
+      if (moveRT()) {
+        incrementState();
       }
       break;
 
-    case S_MOVEUP:
-      stateEnd = moveUP();
-      if (stateEnd) {
-        prev = curr;
-        curr = next;
-        next = S_STOPALL;
+    case S_MOVE_UP:
+      if (moveUP()) {
+        incrementState();
       }
       break;
 
-    case S_MOVEDN:
-      stateEnd = moveDN();
-      if (stateEnd) {
-        prev = curr;
-        curr = next;
-        next = S_STOPALL;
+    case S_MOVE_DN:
+      if (moveDN()) {
+        incrementState();
       }
       break;
 
-    case S_CENTERY:
-      stateEnd = moveMidY();
-      if (stateEnd) {
-        prev = curr;
-        curr = next;
-        next = S_STOPALL;
+    case S_CENTER_X:
+      if (moveMidX()) {
+        incrementState();
+      }
+      break;
+
+    case S_CENTER_Y:
+      if (moveMidY()) {
+        incrementState();
       }
       break;
 
     case S_LIFT_X:
       digitalWrite(xValve, HIGH);
-      stateEnd = liftCupsX();
-      if (stateEnd) {
-        digitalWrite(xValve, LOW);
-        prev = curr;
-        curr = next;
-        switch (next) {
-          case S_STOPALL:
-            next = S_STOPALL;
-            break;
 
-          case S_MOVELT:
-            next = S_LOWERX;
-            break;
-        }
+      if (liftCupsX()) {
+        digitalWrite(xValve, LOW);
+        incrementState();
       }
       break;
 
-    case S_LOWERX:
+    case S_LOWER_X:
       digitalWrite(xValve, LOW);
-      stateEnd = lowerCupsX();
-      if (stateEnd) {
-        prev = curr;
-        curr = next;
-        switch (next) {
-          case S_STOPALL:
-            next = S_STOPALL;
-            break;
 
-          case S_WAITVACX:
-            next = S_LIFT_Y;
-            break;
-        }
+      if (lowerCupsX()) {
+        incrementState();
       }
       break;
 
     case S_LIFT_Y:
       digitalWrite(yValve, HIGH);
-      stateEnd = liftCupsY();
-      if (stateEnd) {
+
+      if (liftCupsY()) {
         digitalWrite(yValve, LOW);
-        prev = curr;
-        curr = next;
-        switch (next) {
-          case S_STOPALL:
-            next = S_STOPALL;
-            break;
-
-          case S_MOVERT:
-            next = S_LOWERY;
-            break;
-        }
+        incrementState();
       }
       break;
 
-    case S_LOWERY:
+    case S_LOWER_Y:
       digitalWrite(yValve, LOW);
-      stateEnd = lowerCupsY();
-      if (stateEnd) {
-        prev = curr;
-        curr = next;
-        switch (next) {
-          case S_STOPALL:
-            next = S_STOPALL;
-            break;
 
-          case S_WAITVACY:
-            next = S_LIFT_X;
-            break;
-        }
+      if (lowerCupsY()) {
+        incrementState();
       }
       break;
 
-    case S_DISPLAY:
-      displayUltrasonic();
-      break;
-
-    case S_STOPALL:
+    case S_STOP_ALL:
       stopAllMotors();
-      prev = curr;
-      curr = next;
-      next = S_STOPALL;
+      incrementState();
       break;
 
-    case S_WAITVACX:
-      stateEnd = waitVacuumX();
-      if (stateEnd) {
-        prev = curr;
-        curr = next;
-        next = S_MOVERT;
+    case S_WAIT_VAC_X:
+      if (waitVacuumX()) {
+        incrementState();
       }
       break;
 
-    case S_WAITVACY:
-      stateEnd = waitVacuumY();
-      if (stateEnd) {
-        prev = curr;
-        curr = next;
-        next = S_MOVELT;
+    case S_WAIT_VAC_Y:
+      if (waitVacuumY()) {
+        incrementState();
       }
       break;
   }
   delay(35);
 }
 
+void incrementState() {
+  currIndex++;
+  currState = *(currManager + currIndex);
+  if (currState == -1) {
+    currIndex = 0;
+    currState = *currManager;
+  }
+  stateCounter = 0;
+}
+
+/********** Actions ***********/
+
 bool waitVacuumX() {
   int sensor = analogRead(xVacuum);
   float voltage = sensor * (5.0 / 1023.0);
   float pressure = (voltage - 2.82) / 0.054;
+
+  if (currDisplayMode == D_GUI) {
+    Serial.print("u");
+    Serial.println(pressure);
+  }
+
   if (pressure < 0) {
     return true;
   } else {
@@ -391,6 +351,12 @@ bool waitVacuumY() {
   int sensor = analogRead(yVacuum);
   float voltage = sensor * (5.0 / 1023.0);
   float pressure = (voltage - 2.82) / 0.054;
+
+  if (currDisplayMode == D_GUI) {
+    Serial.print("v");
+    Serial.println(pressure);
+  }
+
   if (pressure < 0) {
     return true;
   } else {
@@ -398,37 +364,11 @@ bool waitVacuumY() {
   }
 }
 
-void displayPressure() {
-  int sensor1 = analogRead(yVacuum);
-  int sensor2 = analogRead(xVacuum);
-  float voltage1 = sensor1 * (5.0 / 1023.0);
-  float pressure1 = (voltage1 - 2.82) / 0.054;
-  float voltage2 = sensor2 * (5.0 / 1023.0);
-  float pressure2 = (voltage2 - 2.82) / 0.054;
-  Serial.print("PressureY: ");
-  Serial.print(pressure1);
-  Serial.print(" PressureX: ");
-  Serial.println(pressure2);
-  return;
-}
-
-void displayUltrasonic() {
-  long echoLT = sonarLT.ping_median(5);
-  long echoRT = sonarRT.ping_median(5);
-  long echoUP = sonarUP.ping_median(5);
-  long echoDN = sonarDN.ping_median(5);
-  Serial.print("LT=");
-  Serial.print(echoLT);
-  Serial.print(" RT=");
-  Serial.print(echoRT);
-  Serial.print(" UP=");
-  Serial.print(echoUP);
-  Serial.print(" DN=");
-  Serial.println(echoDN);
-  return;
-}
-
 bool moveLT() {
+  if (currDisplayMode == D_GUI) {
+    // TODO: read encoder
+  }
+
   if (digitalRead(pinLimLT) == HIGH) {
     digitalWrite(X1, HIGH);
     digitalWrite(X2, LOW);
@@ -441,6 +381,10 @@ bool moveLT() {
 }
 
 bool moveRT() {
+  if (currDisplayMode == D_GUI) {
+    // TODO: read encoder
+  }
+
   if (digitalRead(pinLimRT) == HIGH) {
     digitalWrite(X1, LOW);
     digitalWrite(X2, HIGH);
@@ -453,6 +397,10 @@ bool moveRT() {
 }
 
 bool moveUP() {
+  if (currDisplayMode == D_GUI) {
+    // TODO: read encoder
+  }
+
   if (digitalRead(pinLimUP) == HIGH) {
     digitalWrite(Y1, LOW);
     digitalWrite(Y2, HIGH);
@@ -465,6 +413,10 @@ bool moveUP() {
 }
 
 bool moveDN() {
+  if (currDisplayMode == D_GUI) {
+    // TODO: read encoder
+  }
+
   if (digitalRead(pinLimDN) == HIGH) {
     digitalWrite(Y1, HIGH);
     digitalWrite(Y2, LOW);
@@ -476,9 +428,19 @@ bool moveDN() {
   }
 }
 
+bool moveMidX() {
+  // TODO
+  return true;
+}
+
 bool moveMidY() {
   int target = yExtreme / 2;
   yPosition = yEncoder.read();
+
+  if (currDisplayMode == D_GUI) {
+    Serial.print("y");
+    Serial.println(yPosition);
+  }
 
   if (target - yPosition > 0) {
     digitalWrite(Y1, LOW);
@@ -501,12 +463,17 @@ bool lowerCupsX() {
   long echoLT = sonarLT.ping_median(2);
   long echoRT = sonarRT.ping_median(2);
 
-  if (printStatus) {
+  if (currDisplayMode == D_DEBUG) {
     Serial.print("LT:");
     Serial.print(echoLT);
     Serial.print("us RT:");
     Serial.print(echoRT);
     Serial.println("us");
+  } else if (currDisplayMode == D_GUI) {
+    Serial.print("e");
+    Serial.println(echoRT);
+    Serial.print("w");
+    Serial.println(echoLT);
   }
 
   if ((echoLT < 250) && (echoRT > 255)) {
@@ -549,12 +516,17 @@ bool liftCupsX() {
   long echoLT = sonarLT.ping_median(2);
   long echoRT = sonarRT.ping_median(2);
 
-  if (printStatus) {
+  if (currDisplayMode == D_DEBUG) {
     Serial.print("LT:");
     Serial.print(echoLT);
     Serial.print("us RT:");
     Serial.print(echoRT);
     Serial.println("us");
+  } else if (currDisplayMode == D_GUI) {
+    Serial.print("e");
+    Serial.println(echoRT);
+    Serial.print("w");
+    Serial.println(echoLT);
   }
 
   if (echoLT - echoRT > 20) {
@@ -598,12 +570,17 @@ bool lowerCupsY() {
   long echoUP = sonarUP.ping_median(2);
   long echoDN = sonarDN.ping_median(2);
 
-  if (printStatus) {
+  if (currDisplayMode == D_DEBUG) {
     Serial.print("UP:");
     Serial.print(echoUP);
     Serial.print("us DN:");
     Serial.print(echoDN);
     Serial.println("us");
+  } else if (currDisplayMode == D_GUI) {
+    Serial.print("n");
+    Serial.println(echoUP);
+    Serial.print("s");
+    Serial.println(echoDN);
   }
 
   if ((echoUP < 230) && (echoDN > 230)) {
@@ -646,12 +623,17 @@ bool liftCupsY() {
   long echoUP = sonarUP.ping_median(2);
   long echoDN = sonarDN.ping_median(2);
 
-  if (printStatus) {
+  if (currDisplayMode == D_DEBUG) {
     Serial.print("UP:");
     Serial.print(echoUP);
     Serial.print("us DN:");
     Serial.print(echoDN);
     Serial.println("us");
+  } else if (currDisplayMode == D_GUI) {
+    Serial.print("n");
+    Serial.println(echoUP);
+    Serial.print("s");
+    Serial.println(echoDN);
   }
 
   if (echoUP < 270) {
@@ -693,15 +675,19 @@ void stopAllMotors() {
   return;
 }
 
+/********** Interrupt Functions ***********/
+
 void hitLT() {
   digitalWrite(X1, LOW);
   digitalWrite(X2, LOW);
+  // xExtreme = xEncoder.read();
   return;
 }
 
 void hitRT() {
   digitalWrite(X1, LOW);
   digitalWrite(X2, LOW);
+  // xEncoder.write(0);
   return;
 }
 
@@ -718,3 +704,36 @@ void hitDN() {
   yEncoder.write(0);
   return;
 }
+
+/********** miscellaneous ***********/
+
+void displayPressure() {
+  int sensor1 = analogRead(yVacuum);
+  int sensor2 = analogRead(xVacuum);
+  float voltage1 = sensor1 * (5.0 / 1023.0);
+  float pressure1 = (voltage1 - 2.82) / 0.054;
+  float voltage2 = sensor2 * (5.0 / 1023.0);
+  float pressure2 = (voltage2 - 2.82) / 0.054;
+  Serial.print("PressureY: ");
+  Serial.print(pressure1);
+  Serial.print(" PressureX: ");
+  Serial.println(pressure2);
+  return;
+}
+
+void displayUltrasonic() {
+  long echoLT = sonarLT.ping_median(5);
+  long echoRT = sonarRT.ping_median(5);
+  long echoUP = sonarUP.ping_median(5);
+  long echoDN = sonarDN.ping_median(5);
+  Serial.print("LT=");
+  Serial.print(echoLT);
+  Serial.print(" RT=");
+  Serial.print(echoRT);
+  Serial.print(" UP=");
+  Serial.print(echoUP);
+  Serial.print(" DN=");
+  Serial.println(echoDN);
+  return;
+}
+
